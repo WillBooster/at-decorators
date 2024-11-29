7567ffc7ac3d10e1d45f24a5939e37cefe67ae3e
Sakamoto, Kazunori <exkazuu@gmail.com>
2024-11-30 00:51:16 +0900

    feat: employ js-sha3 instead of cyrb53

--- src/hash.ts
+++ src/hash.ts
@@ -1,34 +1,449 @@
-// cyrb53 (c) 2018 bryc (github.com/bryc). License: Public domain. Attribution appreciated.
-// A fast and simple 64-bit (or 53-bit) string hash function with decent collision resistance.
-// Largely inspired by MurmurHash2/3, but with a focus on speed/simplicity.
-// See https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript/52171480#52171480
-// https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
-function cyrb64(str: string, seed = 0): [number, number] {
-  let h1 = 0xde_ad_be_ef ^ seed;
-  let h2 = 0x41_c6_ce_57 ^ seed;
-  for (let i = 0, ch; i < str.length; i++) {
-    ch = str.codePointAt(i) as number;
-    h1 = Math.imul(h1 ^ ch, 2_654_435_761);
-    h2 = Math.imul(h2 ^ ch, 1_597_334_677);
+// Copyright 2015-2023 Chen, Yi-Cyuan
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+/**
+ * [js-sha3]{@link https://github.com/emn178/js-sha3}
+ *
+ * @version 0.9.3
+ * @author Chen, Yi-Cyuan [emn178@gmail.com]
+ * @copyright Chen, Yi-Cyuan 2015-2023
+ * @license MIT
+ */
+
+const FINALIZE_ERROR = 'finalize already called';
+const HEX_CHARS = [...'0123456789abcdef'];
+const PADDING = [6, 1536, 393_216, 100_663_296];
+const SHIFT = [0, 8, 16, 24];
+const RC = [
+  1, 0, 32_898, 0, 32_906, 2_147_483_648, 2_147_516_416, 2_147_483_648, 32_907, 0, 2_147_483_649, 0, 2_147_516_545,
+  2_147_483_648, 32_777, 2_147_483_648, 138, 0, 136, 0, 2_147_516_425, 0, 2_147_483_658, 0, 2_147_516_555, 0, 139,
+  2_147_483_648, 32_905, 2_147_483_648, 32_771, 2_147_483_648, 32_770, 2_147_483_648, 128, 2_147_483_648, 32_778, 0,
+  2_147_483_658, 2_147_483_648, 2_147_516_545, 2_147_483_648, 32_896, 2_147_483_648, 2_147_483_649, 0, 2_147_516_424,
+  2_147_483_648,
+];
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export function sha3_512(message: string): string {
+  return new Keccak(512, PADDING, 512).update(message).hex();
+}
+
+const cloneArray = <T>(array: T[]): T[] => {
+  const newArray: T[] = [];
+  for (const [i, element] of array.entries()) {
+    newArray[i] = element;
   }
-  h1 = Math.imul(h1 ^ (h1 >>> 16), 2_246_822_507);
-  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3_266_489_909);
-  h2 = Math.imul(h2 ^ (h2 >>> 16), 2_246_822_507);
-  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3_266_489_909);
-  // For a single 53-bit numeric return value we could return
-  // 4294967296 * (2097151 & h2) + (h1 >>> 0);
-  // but we instead return the full 64-bit value:
-  return [h2 >>> 0, h1 >>> 0];
+  return newArray;
+};
+
+class Keccak {
+  blocks: number[] = [];
+  s: number[] = [];
+  padding: number[];
+  outputBits: number;
+  reset = true;
+  finalized = false;
+  block = 0;
+  start = 0;
+  blockCount: number;
+  byteCount: number;
+  outputBlocks: number;
+  extraBytes: number;
+  lastByteIndex = 0;
+
+  constructor(bits: number, padding: number[], outputBits: number) {
+    this.padding = padding;
+    this.outputBits = outputBits;
+    this.blockCount = (1600 - (bits << 1)) >> 5;
+    this.byteCount = this.blockCount << 2;
+    this.outputBlocks = outputBits >> 5;
+    this.extraBytes = (outputBits & 31) >> 3;
+
+    for (let i = 0; i < 50; ++i) {
+      this.s[i] = 0;
+    }
+  }
+
+  update(message: string): this {
+    if (this.finalized) {
+      throw new Error(FINALIZE_ERROR);
+    }
+    const blocks = this.blocks;
+    const byteCount = this.byteCount;
+    const length = message.length;
+    const blockCount = this.blockCount;
+    const s = this.s;
+    let index = 0;
+    let i: number;
+    let code: number;
+
+    while (index < length) {
+      if (this.reset) {
+        this.reset = false;
+        blocks[0] = this.block;
+        for (i = 1; i < blockCount + 1; ++i) {
+          blocks[i] = 0;
+        }
+      }
+      for (i = this.start; index < length && i < byteCount; ++index) {
+        code = message.codePointAt(index) || 0;
+        if (code < 0x80) {
+          blocks[i >> 2] |= code << SHIFT[i++ & 3];
+        } else if (code < 0x8_00) {
+          blocks[i >> 2] |= (0xc0 | (code >> 6)) << SHIFT[i++ & 3];
+          blocks[i >> 2] |= (0x80 | (code & 0x3f)) << SHIFT[i++ & 3];
+        } else if (code < 0xd8_00 || code >= 0xe0_00) {
+          blocks[i >> 2] |= (0xe0 | (code >> 12)) << SHIFT[i++ & 3];
+          blocks[i >> 2] |= (0x80 | ((code >> 6) & 0x3f)) << SHIFT[i++ & 3];
+          blocks[i >> 2] |= (0x80 | (code & 0x3f)) << SHIFT[i++ & 3];
+        } else {
+          code = 0x1_00_00 + (((code & 0x3_ff) << 10) | ((message.codePointAt(++index) || 0) & 0x3_ff));
+          blocks[i >> 2] |= (0xf0 | (code >> 18)) << SHIFT[i++ & 3];
+          blocks[i >> 2] |= (0x80 | ((code >> 12) & 0x3f)) << SHIFT[i++ & 3];
+          blocks[i >> 2] |= (0x80 | ((code >> 6) & 0x3f)) << SHIFT[i++ & 3];
+          blocks[i >> 2] |= (0x80 | (code & 0x3f)) << SHIFT[i++ & 3];
+        }
+      }
+      this.lastByteIndex = i;
+      if (i >= byteCount) {
+        this.start = i - byteCount;
+        this.block = blocks[blockCount];
+        for (i = 0; i < blockCount; ++i) {
+          s[i] ^= blocks[i];
+        }
+        f(s);
+        this.reset = true;
+      } else {
+        this.start = i;
+      }
+    }
+    return this;
+  }
+
+  finalize(): void {
+    if (this.finalized) {
+      return;
+    }
+    this.finalized = true;
+    const blocks = this.blocks;
+    let i = this.lastByteIndex;
+    const blockCount = this.blockCount;
+    const s = this.s;
+    blocks[i >> 2] |= this.padding[i & 3];
+    if (this.lastByteIndex === this.byteCount) {
+      blocks[0] = blocks[blockCount];
+      for (i = 1; i < blockCount + 1; ++i) {
+        blocks[i] = 0;
+      }
+    }
+    blocks[blockCount - 1] |= 0x80_00_00_00;
+    for (i = 0; i < blockCount; ++i) {
+      s[i] ^= blocks[i];
+    }
+    f(s);
+  }
+
+  hex(): string {
+    this.finalize();
+
+    const blockCount = this.blockCount;
+    let s = this.s;
+    const outputBlocks = this.outputBlocks;
+    const extraBytes = this.extraBytes;
+    let i = 0;
+    let j = 0;
+    let hex = '';
+    let block: number;
+    while (j < outputBlocks) {
+      for (i = 0; i < blockCount && j < outputBlocks; ++i, ++j) {
+        block = s[i];
+        hex +=
+          HEX_CHARS[(block >> 4) & 0x0f] +
+          HEX_CHARS[block & 0x0f] +
+          HEX_CHARS[(block >> 12) & 0x0f] +
+          HEX_CHARS[(block >> 8) & 0x0f] +
+          HEX_CHARS[(block >> 20) & 0x0f] +
+          HEX_CHARS[(block >> 16) & 0x0f] +
+          HEX_CHARS[(block >> 28) & 0x0f] +
+          HEX_CHARS[(block >> 24) & 0x0f];
+      }
+      if (j % blockCount === 0) {
+        s = cloneArray(s);
+        f(s);
+        i = 0;
+      }
+    }
+    if (extraBytes) {
+      block = s[i];
+      hex += HEX_CHARS[(block >> 4) & 0x0f] + HEX_CHARS[block & 0x0f];
+      if (extraBytes > 1) {
+        hex += HEX_CHARS[(block >> 12) & 0x0f] + HEX_CHARS[(block >> 8) & 0x0f];
+      }
+      if (extraBytes > 2) {
+        hex += HEX_CHARS[(block >> 20) & 0x0f] + HEX_CHARS[(block >> 16) & 0x0f];
+      }
+    }
+    return hex;
+  }
 }
 
-// An improved, *insecure* 64-bit hash that's short, fast, and has no dependencies.
-// Output is always 14 characters.
-// export function cyrb64Hash(str: string, seed = 0): string {
-//   const [h2, h1] = cyrb64(str, seed);
-//   return h2.toString(36).padStart(7, '0') + h1.toString(36).padStart(7, '0');
-// }
+const f = function (s: number[]): void {
+  let h: number;
+  let l: number;
+  let n: number;
+  let c0: number;
+  let c1: number;
+  let c2: number;
+  let c3: number;
+  let c4: number;
+  let c5: number;
+  let c6: number;
+  let c7: number;
+  let c8: number;
+  let c9: number;
+  let b0: number;
+  let b1: number;
+  let b2: number;
+  let b3: number;
+  let b4: number;
+  let b5: number;
+  let b6: number;
+  let b7: number;
+  let b8: number;
+  let b9: number;
+  let b10: number;
+  let b11: number;
+  let b12: number;
+  let b13: number;
+  let b14: number;
+  let b15: number;
+  let b16: number;
+  let b17: number;
+  let b18: number;
+  let b19: number;
+  let b20: number;
+  let b21: number;
+  let b22: number;
+  let b23: number;
+  let b24: number;
+  let b25: number;
+  let b26: number;
+  let b27: number;
+  let b28: number;
+  let b29: number;
+  let b30: number;
+  let b31: number;
+  let b32: number;
+  let b33: number;
+  let b34: number;
+  let b35: number;
+  let b36: number;
+  let b37: number;
+  let b38: number;
+  let b39: number;
+  let b40: number;
+  let b41: number;
+  let b42: number;
+  let b43: number;
+  let b44: number;
+  let b45: number;
+  let b46: number;
+  let b47: number;
+  let b48: number;
+  let b49: number;
+  for (n = 0; n < 48; n += 2) {
+    c0 = s[0] ^ s[10] ^ s[20] ^ s[30] ^ s[40];
+    c1 = s[1] ^ s[11] ^ s[21] ^ s[31] ^ s[41];
+    c2 = s[2] ^ s[12] ^ s[22] ^ s[32] ^ s[42];
+    c3 = s[3] ^ s[13] ^ s[23] ^ s[33] ^ s[43];
+    c4 = s[4] ^ s[14] ^ s[24] ^ s[34] ^ s[44];
+    c5 = s[5] ^ s[15] ^ s[25] ^ s[35] ^ s[45];
+    c6 = s[6] ^ s[16] ^ s[26] ^ s[36] ^ s[46];
+    c7 = s[7] ^ s[17] ^ s[27] ^ s[37] ^ s[47];
+    c8 = s[8] ^ s[18] ^ s[28] ^ s[38] ^ s[48];
+    c9 = s[9] ^ s[19] ^ s[29] ^ s[39] ^ s[49];
 
-export function cyrb64HashWithLength(str: string, seed = 0): string {
-  const [h2, h1] = cyrb64(str, seed);
-  return str.length.toString(36) + h2.toString(36).padStart(7, '0') + h1.toString(36).padStart(7, '0');
-}
+    h = c8 ^ ((c2 << 1) | (c3 >>> 31));
+    l = c9 ^ ((c3 << 1) | (c2 >>> 31));
+    s[0] ^= h;
+    s[1] ^= l;
+    s[10] ^= h;
+    s[11] ^= l;
+    s[20] ^= h;
+    s[21] ^= l;
+    s[30] ^= h;
+    s[31] ^= l;
+    s[40] ^= h;
+    s[41] ^= l;
+    h = c0 ^ ((c4 << 1) | (c5 >>> 31));
+    l = c1 ^ ((c5 << 1) | (c4 >>> 31));
+    s[2] ^= h;
+    s[3] ^= l;
+    s[12] ^= h;
+    s[13] ^= l;
+    s[22] ^= h;
+    s[23] ^= l;
+    s[32] ^= h;
+    s[33] ^= l;
+    s[42] ^= h;
+    s[43] ^= l;
+    h = c2 ^ ((c6 << 1) | (c7 >>> 31));
+    l = c3 ^ ((c7 << 1) | (c6 >>> 31));
+    s[4] ^= h;
+    s[5] ^= l;
+    s[14] ^= h;
+    s[15] ^= l;
+    s[24] ^= h;
+    s[25] ^= l;
+    s[34] ^= h;
+    s[35] ^= l;
+    s[44] ^= h;
+    s[45] ^= l;
+    h = c4 ^ ((c8 << 1) | (c9 >>> 31));
+    l = c5 ^ ((c9 << 1) | (c8 >>> 31));
+    s[6] ^= h;
+    s[7] ^= l;
+    s[16] ^= h;
+    s[17] ^= l;
+    s[26] ^= h;
+    s[27] ^= l;
+    s[36] ^= h;
+    s[37] ^= l;
+    s[46] ^= h;
+    s[47] ^= l;
+    h = c6 ^ ((c0 << 1) | (c1 >>> 31));
+    l = c7 ^ ((c1 << 1) | (c0 >>> 31));
+    s[8] ^= h;
+    s[9] ^= l;
+    s[18] ^= h;
+    s[19] ^= l;
+    s[28] ^= h;
+    s[29] ^= l;
+    s[38] ^= h;
+    s[39] ^= l;
+    s[48] ^= h;
+    s[49] ^= l;
+
+    b0 = s[0];
+    b1 = s[1];
+    b32 = (s[11] << 4) | (s[10] >>> 28);
+    b33 = (s[10] << 4) | (s[11] >>> 28);
+    b14 = (s[20] << 3) | (s[21] >>> 29);
+    b15 = (s[21] << 3) | (s[20] >>> 29);
+    b46 = (s[31] << 9) | (s[30] >>> 23);
+    b47 = (s[30] << 9) | (s[31] >>> 23);
+    b28 = (s[40] << 18) | (s[41] >>> 14);
+    b29 = (s[41] << 18) | (s[40] >>> 14);
+    b20 = (s[2] << 1) | (s[3] >>> 31);
+    b21 = (s[3] << 1) | (s[2] >>> 31);
+    b2 = (s[13] << 12) | (s[12] >>> 20);
+    b3 = (s[12] << 12) | (s[13] >>> 20);
+    b34 = (s[22] << 10) | (s[23] >>> 22);
+    b35 = (s[23] << 10) | (s[22] >>> 22);
+    b16 = (s[33] << 13) | (s[32] >>> 19);
+    b17 = (s[32] << 13) | (s[33] >>> 19);
+    b48 = (s[42] << 2) | (s[43] >>> 30);
+    b49 = (s[43] << 2) | (s[42] >>> 30);
+    b40 = (s[5] << 30) | (s[4] >>> 2);
+    b41 = (s[4] << 30) | (s[5] >>> 2);
+    b22 = (s[14] << 6) | (s[15] >>> 26);
+    b23 = (s[15] << 6) | (s[14] >>> 26);
+    b4 = (s[25] << 11) | (s[24] >>> 21);
+    b5 = (s[24] << 11) | (s[25] >>> 21);
+    b36 = (s[34] << 15) | (s[35] >>> 17);
+    b37 = (s[35] << 15) | (s[34] >>> 17);
+    b18 = (s[45] << 29) | (s[44] >>> 3);
+    b19 = (s[44] << 29) | (s[45] >>> 3);
+    b10 = (s[6] << 28) | (s[7] >>> 4);
+    b11 = (s[7] << 28) | (s[6] >>> 4);
+    b42 = (s[17] << 23) | (s[16] >>> 9);
+    b43 = (s[16] << 23) | (s[17] >>> 9);
+    b24 = (s[26] << 25) | (s[27] >>> 7);
+    b25 = (s[27] << 25) | (s[26] >>> 7);
+    b6 = (s[36] << 21) | (s[37] >>> 11);
+    b7 = (s[37] << 21) | (s[36] >>> 11);
+    b38 = (s[47] << 24) | (s[46] >>> 8);
+    b39 = (s[46] << 24) | (s[47] >>> 8);
+    b30 = (s[8] << 27) | (s[9] >>> 5);
+    b31 = (s[9] << 27) | (s[8] >>> 5);
+    b12 = (s[18] << 20) | (s[19] >>> 12);
+    b13 = (s[19] << 20) | (s[18] >>> 12);
+    b44 = (s[29] << 7) | (s[28] >>> 25);
+    b45 = (s[28] << 7) | (s[29] >>> 25);
+    b26 = (s[38] << 8) | (s[39] >>> 24);
+    b27 = (s[39] << 8) | (s[38] >>> 24);
+    b8 = (s[48] << 14) | (s[49] >>> 18);
+    b9 = (s[49] << 14) | (s[48] >>> 18);
+
+    s[0] = b0 ^ (~b2 & b4);
+    s[1] = b1 ^ (~b3 & b5);
+    s[10] = b10 ^ (~b12 & b14);
+    s[11] = b11 ^ (~b13 & b15);
+    s[20] = b20 ^ (~b22 & b24);
+    s[21] = b21 ^ (~b23 & b25);
+    s[30] = b30 ^ (~b32 & b34);
+    s[31] = b31 ^ (~b33 & b35);
+    s[40] = b40 ^ (~b42 & b44);
+    s[41] = b41 ^ (~b43 & b45);
+    s[2] = b2 ^ (~b4 & b6);
+    s[3] = b3 ^ (~b5 & b7);
+    s[12] = b12 ^ (~b14 & b16);
+    s[13] = b13 ^ (~b15 & b17);
+    s[22] = b22 ^ (~b24 & b26);
+    s[23] = b23 ^ (~b25 & b27);
+    s[32] = b32 ^ (~b34 & b36);
+    s[33] = b33 ^ (~b35 & b37);
+    s[42] = b42 ^ (~b44 & b46);
+    s[43] = b43 ^ (~b45 & b47);
+    s[4] = b4 ^ (~b6 & b8);
+    s[5] = b5 ^ (~b7 & b9);
+    s[14] = b14 ^ (~b16 & b18);
+    s[15] = b15 ^ (~b17 & b19);
+    s[24] = b24 ^ (~b26 & b28);
+    s[25] = b25 ^ (~b27 & b29);
+    s[34] = b34 ^ (~b36 & b38);
+    s[35] = b35 ^ (~b37 & b39);
+    s[44] = b44 ^ (~b46 & b48);
+    s[45] = b45 ^ (~b47 & b49);
+    s[6] = b6 ^ (~b8 & b0);
+    s[7] = b7 ^ (~b9 & b1);
+    s[16] = b16 ^ (~b18 & b10);
+    s[17] = b17 ^ (~b19 & b11);
+    s[26] = b26 ^ (~b28 & b20);
+    s[27] = b27 ^ (~b29 & b21);
+    s[36] = b36 ^ (~b38 & b30);
+    s[37] = b37 ^ (~b39 & b31);
+    s[46] = b46 ^ (~b48 & b40);
+    s[47] = b47 ^ (~b49 & b41);
+    s[8] = b8 ^ (~b0 & b2);
+    s[9] = b9 ^ (~b1 & b3);
+    s[18] = b18 ^ (~b10 & b12);
+    s[19] = b19 ^ (~b11 & b13);
+    s[28] = b28 ^ (~b20 & b22);
+    s[29] = b29 ^ (~b21 & b23);
+    s[38] = b38 ^ (~b30 & b32);
+    s[39] = b39 ^ (~b31 & b33);
+    s[48] = b48 ^ (~b40 & b42);
+    s[49] = b49 ^ (~b41 & b43);
+
+    s[0] ^= RC[n];
+    s[1] ^= RC[n + 1];
+  }
+};
