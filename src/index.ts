3ad761db1ddd8323be8e2c469baea173d5c0d2ea
Sakamoto, Kazunori <exkazuu@gmail.com>
2025-01-02 13:41:13 +0900

    fix: export memoizeWithPersistentCacheFactory

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { calcHashWithContext, calcEmptyHash } from './caclHash.js';
 export { sha3_512 } from './hash.js';
 export { memoize, memoizeFactory } from './memoize.js';
 export { memoizeOne, memoizeOneWithEmptyHash, memoizeOneFactory } from './memoizeOne.js';
+export { memoizeWithPersistentCacheFactory } from './memoizeWithPersistentCache.js';
